fdc2ea8f5b6b48e68c45ee141eab2793120e79e0
Bodo Junglas <bodo.junglas.extern@zalando.de>
2016-03-08 11:45:50 +0100

    Improve coverage a bit

--- prop/convert_result_test.go
+++ prop/convert_result_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/leanovate/gopter"
 )
 
-func TestCheckCondition(t *testing.T) {
+func TestConvertResult(t *testing.T) {
 	trueResult := convertResult(true, nil)
 	if trueResult.Status != gopter.PropTrue || trueResult.Error != nil {
 		t.Errorf("Invalid true result: %#v", trueResult)
@@ -26,7 +26,12 @@ func TestCheckCondition(t *testing.T) {
 	propResult := convertResult(&gopter.PropResult{
 		Status: gopter.PropProof,
 	}, nil)
-	if propResult.Status != gopter.PropProof || falseResult.Error != nil {
+	if propResult.Status != gopter.PropProof || propResult.Error != nil {
 		t.Errorf("Invalid prop result: %#v", propResult)
+	}
+
+	invalidResult := convertResult(0, nil)
+	if invalidResult.Status != gopter.PropError || invalidResult.Error == nil {
+		t.Errorf("Invalid prop result: %#v", invalidResult)
 	}
 }
