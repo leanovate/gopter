a76dacf50d2fd0da6c0bf1578579c2af844ad33d
Prateek Rungta <prungta2@gmail.com>
2018-06-22 16:42:30 -0400

    Allow users to override reporter  during TestingRun

--- properties_test.go
+++ properties_test.go
@@ -1,6 +1,7 @@
 package gopter_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/leanovate/gopter"
@@ -22,6 +23,24 @@ func TestProperties(t *testing.T) {
 
 	fakeT := &testing.T{}
 	properties.TestingRun(fakeT)
+	if !fakeT.Failed() {
+		t.Errorf("fakeT has not failed")
+	}
+}
+
+func TestPropertiesCustomReporter(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+
+	properties.Property("always fail", prop.ForAll(
+		func(v int32) bool {
+			return false
+		},
+		gen.Int32(),
+	))
+
+	fakeT := &testing.T{}
+	properties.TestingRun(fakeT, gopter.NewFormatedReporter(true, 160, os.Stdout))
 	if !fakeT.Failed() {
 		t.Errorf("fakeT has not failed")
 	}
