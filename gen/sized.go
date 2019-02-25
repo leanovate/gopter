d8dd4fa618c198cd75283e7acfd8d67ff650094a
Bodo Junglas <junglas@objectcode.de>
2019-02-25 11:20:43 +0100

    Fix MaxSize=MinSize case

--- gen/sized.go
+++ gen/sized.go
@@ -6,9 +6,15 @@ import (
 
 // Sized derives a generator from based on size
 // This honors the `MinSize` and `MaxSize` of the `GenParameters` of the test suite.
+// Keep an eye on memory consumption, by default MaxSize is 100.
 func Sized(f func(int) gopter.Gen) gopter.Gen {
 	return func(params *gopter.GenParameters) *gopter.GenResult {
-		size := params.Rng.Intn(params.MaxSize-params.MinSize) + params.MinSize
+		var size int
+		if params.MaxSize == params.MinSize {
+			size = params.MaxSize
+		} else {
+			size = params.Rng.Intn(params.MaxSize-params.MinSize) + params.MinSize
+		}
 		return f(size)(params)
 	}
 }
